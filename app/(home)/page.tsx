e319e420bb7aba2b3e06e16ccc6f241b434b48f2
Akramjanov Asadbke <kosmoc44@gmail.com>
2025-04-08 00:17:34 +0500

    del searchparams

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import AccessDenied from "@/components/AccessDenied";
 import LINKButtons from "@/components/LINKButtons";
 import { AccessibilityIcon, BarChart2Icon, CogIcon } from "lucide-react";
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 const features = [
@@ -32,67 +29,50 @@ const features = [
 ];
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const [error, setError] = useState<string | null>(null);
-
-  // Используем useEffect для обработки параметров на клиентской стороне
-  useEffect(() => {
-    setError(searchParams.get("error"));
-  }, [searchParams]);
-
   return (
     <main>
-      {error ? (
-        <AccessDenied />
-      ) : (
-        <>
-          <div className="w-full py-12 md:py-24 lg:py-32">
-            <div className="container px-4 md:px-6">
-              <div className="flex flex-col items-center text-center justify-center space-y-4">
-                <div className="space-y-2">
-                  <h1 className="text-3xl md:text-5xl font-bold text-center">
-                    Welcome to Project Forms
-                  </h1>
-                  <p className="text-2xl md:text-xl text-center text-gray-500">
-                    Create and manage your forms effortlessly
-                  </p>
-                  <LINKButtons />
-                </div>
-              </div>
+      <div className="w-full py-12 md:py-24 lg:py-32">
+        <div className="container px-4 md:px-6">
+          <div className="flex flex-col items-center text-center justify-center space-y-4">
+            <div className="space-y-2">
+              <h1 className="text-3xl md:text-5xl font-bold text-center">
+                Welcome to Project Forms
+              </h1>
+              <p className="text-2xl md:text-xl text-center text-gray-500">
+                Create and manage your forms effortlessly
+              </p>
+              <LINKButtons />
             </div>
-            <section className="w-full py-12 md:py-24 lg:pt-50">
-              <div className="container grid items-center gap-4 px-4 md:px-6 lg:gap-10">
-                <div className="space-y-3">
-                  <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-                    Create Forms with Ease
-                  </h2>
-                  <p className="text-lg md:text-xl max-w-[700px] text-gray-500 md:text-xl-/relaxed">
-                    Our platform allows you to create forms quickly and
-                    efficiently, with a user-friendly interface and powerful
-                    features.
-                  </p>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                  {features.map((feature) => (
-                    <Card key={feature.id} className="shadow-md">
-                      <CardHeader className="flex items-center justify-between">
-                        <h3 className="text-xl font-semibold">
-                          {feature.title}
-                        </h3>
-                        <div>{feature.Icon}</div>
-                      </CardHeader>
-                      <CardContent>
-                        <p className="text-gray-500">{feature.description}</p>
-                      </CardContent>
-                    </Card>
-                  ))}
-                </div>
-                <LINKButtons className="mt-8" />
-              </div>
-            </section>
           </div>
-        </>
-      )}
+        </div>
+        <section className="w-full py-12 md:py-24 lg:pt-50">
+          <div className="container grid items-center gap-4 px-4 md:px-6 lg:gap-10">
+            <div className="space-y-3">
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+                Create Forms with Ease
+              </h2>
+              <p className="text-lg md:text-xl max-w-[700px] text-gray-500 md:text-xl-/relaxed">
+                Our platform allows you to create forms quickly and efficiently,
+                with a user-friendly interface and powerful features.
+              </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {features.map((feature) => (
+                <Card key={feature.id} className="shadow-md">
+                  <CardHeader className="flex items-center justify-between">
+                    <h3 className="text-xl font-semibold">{feature.title}</h3>
+                    <div>{feature.Icon}</div>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-gray-500">{feature.description}</p>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+            <LINKButtons className="mt-8" />
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
