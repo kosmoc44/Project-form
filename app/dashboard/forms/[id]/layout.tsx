4cb23f902306c1f497674c511c6517c4fc188102
Akramjanov Asadbke <kosmoc44@gmail.com>
2025-04-11 19:52:20 +0500

    R

--- app/dashboard/forms/[id]/layout.tsx
+++ app/dashboard/forms/[id]/layout.tsx
@@ -1,26 +1,11 @@
-import FormPageHeader from "@/components/FormPageHeader";
-import { fetchFormById } from "@/lib/data";
-import { notFound } from "next/navigation";
-
-async function FormIdLayout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: { id: string };
-}) {
-  const form = await fetchFormById(params.id);
-
-  if (!form?.id) {
-    notFound();
+export default function FormIdLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <>
+        <div className="bg-violet-100 min-h-screen">{children}</div>
+      </>
+    );
   }
-
-  return (
-    <>
-      <FormPageHeader form={form} />
-      <div className="bg-violet-100 min-h-screen">{children}</div>
-    </>
-  );
-}
-
-export default FormIdLayout;
--- app/dashboard/forms/[id]/page.tsx
+++ app/dashboard/forms/[id]/page.tsx
@@ -1,6 +1,18 @@
+import FormPageHeader from "@/components/FormPageHeader";
+import { fetchFormById } from "@/lib/data";
+import { notFound } from "next/navigation";
 
-function FormPage() {
-  return <div>FormPage</div>;
-}
-
-export default FormPage;
+export default async function FormPage({ params }: { params: { id: string } }) {
+    const result = await fetchFormById(params.id);
+  
+    if ("error" in result || !result?.id) {
+      notFound();
+    }
+  
+    return (
+      <>
+        <FormPageHeader form={result} />
+        <div className="p-4">Form Content Goes Here</div>
+      </>
+    );
+  }
